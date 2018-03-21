84b4e73478dbbb1e0129769e054d19bc732cfbf2
Zach DeCook <zachdecook@gmail.com>
2018-03-21 11:56:59 -0400

    * Make command handle additional csv columns

--- commands.sh
+++ commands.sh
@@ -5,11 +5,11 @@
 if [ "$(uname)" == "Darwin" ]; then
 	# sed on macos doesn't like \t, needs verbatim tab
 	cat -n chords.csv | sed 's/^ *\([0-9]*\)	/\1,/g' |
-	 sort -t',' -k2,2 -k1,1 | sed "s/.*,\(.*,.*\)/\1/" |
+	 sort -t',' -k2,2 -k1,1 | sed "s/[^,]*,\([^,]*,.*\)/\1/" |
 	 uniq | perl tojson.pl
 else
 	# sed on unix (gnu-sed) is fine with \t
 	cat -n chords.csv | sed 's/^ *\([0-9]*\)\t/\1,/g' |
-	 sort -t',' -k2,2 -k1,1 | sed "s/.*,\(.*,.*\)/\1/" |
+	 sort -t',' -k2,2 -k1,1 | sed "s/[^,]*,\([^,]*,.*\)/\1/" |
 	 uniq | perl tojson.pl
 fi
