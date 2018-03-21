325a208e6bbc9f53fc1e503489231ded251eb095
Zach DeCook <zachdecook@gmail.com>
2018-03-21 11:07:13 -0400

    * Create scripts for parsing csv into json

---
+++ commands.sh
@@ -0,0 +1,15 @@
+# Sorts chords.csv, pipes through tojson.pl
+# Run like ./commands.sh > chords.json
+# Author: Zach DeCook
+
+if [ "$(uname)" == "Darwin" ]; then
+	# sed on macos doesn't like \t, needs verbatim tab
+	cat -n chords.csv | sed 's/^ *\([0-9]*\)	/\1,/g' |
+	 sort -t',' -k2,2 -k1,1 | sed "s/.*,\(.*,.*\)/\1/" |
+	 uniq | perl tojson.pl
+else
+	# sed on unix (gnu-sed) is fine with \t
+	cat -n chords.csv | sed 's/^ *\([0-9]*\)\t/\1,/g' |
+	 sort -t',' -k2,2 -k1,1 | sed "s/.*,\(.*,.*\)/\1/" |
+	 uniq | perl tojson.pl
+fi
---
+++ tojson.pl
@@ -0,0 +1,28 @@
+# Parse sorted csv (key,value) into (json 'key' : ['value1', 'value2'],)
+# Author: Zach DeCook
+
+# previous name
+$ln = "firstrun";
+
+print "{";
+
+# Read from stdin or from file passed as argument
+while(<>){
+	($name, $tab) = /(.*),(.*)\n/g;
+	if ( $name ne $ln ){
+		if ( $ln ne "firstrun" ){
+			print "],\n";
+		}
+		print "'$name' : ['$tab'";
+	}
+	else {
+		print ", '$tab'";
+	}
+	$ln = $name;
+}
+
+if ( $ln ne "firstrun" ){
+	print "]"
+}
+
+print "}\n";
